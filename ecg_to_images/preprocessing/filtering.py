correct filtering and normalization for logarithmic scale, add tests

ecg_to_images/preprocessing/filtering.py
@@ -10,32 +10,17 @@ def remove_range_outliers(patient_array: np.ndarray, options) -> np.ndarray:
 
 
 def rr_fixed_range(patient_array):
-    it = 0
-    # tested values with r script. At least 80% of the original population is left
-    # after outlier removal
-    min = 0.2
-    max = 1.5
-    values_in_range = []
-
-    # Keep values within [0.2, 1.5] range
-    # while it < patient_array.size:
-    #
-    #     if min <= patient_array[it] <= max:
-    #         values_in_range = np.append(values_in_range, patient_array[it])
-    #
-    #     it += 1
-    # return values_in_range
-    test = np.delete(patient_array, np.argwhere((patient_array < 0.2) & (patient_array > 1.5)))
-    return test
+    # tested values with r script. At least 80% of the original population is left after outlier removal
+    return np.delete(patient_array, np.argwhere((patient_array < 0.2) | (patient_array > 1.5)))
 
 def rr_mean_std_range(patient_array):
 
-    it = 0
     pa_mean = np.mean(patient_array)
     pa_std = np.std(patient_array)
     values_in_range = []
 
     # Keep values within [mean +- 2 * standard deviation] range
+    it = 0
     while it < patient_array.size:
 
         if patient_array[it] < pa_mean:

ecg_to_images/preprocessing/preprocessing.py
@@ -22,7 +22,7 @@ def preprocessing(patient_array, options) -> np.ndarray:
     if rescale_conf == 'normalize':
        rescaled_pa = normalize(filtered_pa)
     elif rescale_conf == 'normalize_to_byte_image':
-        rescaled_pa = normalize_to_byte_img(filtered_pa, options)
+        rescaled_pa = normalize_to_byte_img(filtered_pa)
     elif rescale_conf == 'standardize':
         rescaled_pa = standardize(filtered_pa)
     else:

ecg_to_images/tests/preprocessing/test_filtering.py
@@ -3,7 +3,7 @@ import logging
 import numpy as np
 import pytest
 
-from ecg_to_images.preprocessing.filtering import rr_range
+from ecg_to_images.preprocessing.filtering import rr_fixed_range, rr_mean_std_range
 
 logger = logging.getLogger(__name__)
 
@@ -11,25 +11,30 @@ logger = logging.getLogger(__name__)
 @pytest.fixture(scope='module')
 def read_array():
     try:
-        patient_array = np.genfromtxt("/home/george/Dropbox/shareedb/RRouts/rr02012.txt", delimiter='\n',
+        patient_array = np.genfromtxt("/home/george/Data/RR intervals/shareedb/RR-no-neg/", delimiter='\n',
                                       dtype=np.float64)
     except Exception as err:
         print("Error in create_images method: " + err)
 
-    return patient_array, rr_range(patient_array)
+    return patient_array
+
 
 # different way to create the filtered array
 def test_rr_range(read_array):
-    patient_array, range_pa  = read_array
+    patient_array = read_array
+    range_pa = rr_mean_std_range(patient_array)
 
-    index_to_delete = []
-    it = 0
     pa_mean = np.mean(patient_array)
     pa_std = np.std(patient_array)
+    index_to_delete = []
+
+    it = 0
     while it < patient_array.size:
+
         if patient_array[it] >= pa_mean:
             if patient_array[it] > (pa_mean + (2 * pa_std)):
                 index_to_delete.extend([it])
+
         elif patient_array[it] < pa_mean:
             if patient_array[it] < (pa_mean - (2 * pa_std)):
                 index_to_delete.extend([it])
@@ -37,3 +42,23 @@ def test_rr_range(read_array):
         it += 1
 
     assert np.array_equal(np.delete(patient_array, index_to_delete), range_pa), "arrays should be same"
+
+
+def test_rr_fixed_range(read_array):
+    patient_array = read_array
+    fixed_range = rr_fixed_range(patient_array)
+
+    min = 0.2
+    max = 1.5
+    values_in_range = []
+
+    # Keep values within [0.2, 1.5] range
+    it = 0
+    while it < patient_array.size:
+
+        if min <= patient_array[it] <= max:
+            values_in_range = np.append(values_in_range, patient_array[it])
+
+        it += 1
+
+    assert np.array_equal(fixed_range, values_in_range), "arrays should be same"
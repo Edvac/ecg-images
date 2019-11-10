correct log scaling and refactor filtering and normalization modules

ecg_to_images/preprocessing/filtering.py
@@ -1,12 +1,34 @@
 import numpy as np
 
 
-def remove_rr_outliers(patient_array: np.ndarray) -> np.ndarray:
-    patient_arr_up_thr = rr_range(patient_array)
-    return patient_arr_up_thr
+def remove_range_outliers(patient_array: np.ndarray, options) -> np.ndarray:
+    if options.get("preprocessing", "range") == 'fixed':
+        patient_ar = rr_fixed_range(patient_array)
+    elif options.get("preprocessing", "range") == 'mean_std':
+        patient_ar = rr_mean_std_range(patient_array)
+    return patient_ar
 
 
-def rr_range(patient_array):
+def rr_fixed_range(patient_array):
+    it = 0
+    # tested values with r script. At least 80% of the original population is left
+    # after outlier removal
+    min = 0.2
+    max = 1.5
+    values_in_range = []
+
+    # Keep values within [0.2, 1.5] range
+    # while it < patient_array.size:
+    #
+    #     if min <= patient_array[it] <= max:
+    #         values_in_range = np.append(values_in_range, patient_array[it])
+    #
+    #     it += 1
+    # return values_in_range
+    test = np.delete(patient_array, np.argwhere((patient_array < 0.2) & (patient_array > 1.5)))
+    return test
+
+def rr_mean_std_range(patient_array):
 
     it = 0
     pa_mean = np.mean(patient_array)

ecg_to_images/preprocessing/normalize_to_byte_img.py
@@ -3,38 +3,23 @@ import numpy as np
 
 def normalize_to_byte_img(filtered_patient_array, options):
     """This function rescales the filtered values into [0,255] interval.
+    If the scale is linear:
+
     .. math::
-        f(n)=\\frac{255\\times log_{10}n}{\\log_{10}max-\\log_{10}min}
+        f(n)=\\frac{255\\times n - min}{max-min}
+
+    If the scale is logarithmic:
+
+    .. math::
+        f(n)=\\frac{255\\times log_{2}n - log_{2}min}{\\log_{2}max-\\log_{2}min}
 
     Args:
         filtered_patient_array (ndarray): removed negatives and values in placed in range
         options (file): config file
-built
+
     Returns: an rescaled integer numpy array
-
-    References: log scale conversion is based on: https://math.stackexchange.com/questions/970094/convert-a-linear-scale-to-a-logarithmic-scale
-    and https://www.youtube.com/watch?v=sBhEi4L91Sg
     """
-    scale_conf = options.get("preprocessing", "scale")
-    if scale_conf == "linear":
-        return linear_scale(filtered_patient_array)
-    else:
-        return log_scale(filtered_patient_array, scale_conf)
-
-def linear_scale(filtered_patient_array):
     return (255 * ((filtered_patient_array - np.min(filtered_patient_array))
                    / np.ptp(filtered_patient_array))).astype(np.uint8)
 
 
-def log_scale(fltrd_pa, scale):
-    if scale == "log2":
-        return ((255 * np.log2(fltrd_pa))
-                / np.log2(np.max(fltrd_pa)) - np.log2(np.min(fltrd_pa))).astype(np.uint8)
-
-    elif scale == "loge":
-        return ((255 * np.log(fltrd_pa))
-                / np.log(np.max(fltrd_pa)) - np.log(np.min(fltrd_pa))).astype(np.uint8)
-
-    elif scale == "log10":
-        return ((255 * np.log10(fltrd_pa))
-                / np.log10(np.max(fltrd_pa)) - np.log10(np.min(fltrd_pa))).astype(np.uint8)

ecg_to_images/preprocessing/preprocessing.py
@@ -1,6 +1,7 @@
 import numpy as np
 
-from ecg_to_images.preprocessing.filtering import remove_rr_outliers
+from ecg_to_images.preprocessing.filtering import remove_range_outliers
+from ecg_to_images.preprocessing.linear_to_log import log_scale
 from ecg_to_images.preprocessing.normalize import normalize
 from ecg_to_images.preprocessing.normalize_to_byte_img import normalize_to_byte_img
 from ecg_to_images.preprocessing.remove_negative_values import remove_negative_values
@@ -10,7 +11,11 @@ from ecg_to_images.preprocessing.standardize import standardize
 def preprocessing(patient_array, options) -> np.ndarray:
 
     positive_pa = remove_negative_values(patient_array)
-    filtered_pa = remove_rr_outliers(positive_pa)
+    filtered_pa = remove_range_outliers(positive_pa, options)
+
+    scale_conf = options.get("preprocessing", "scale")
+    if scale_conf != "linear":
+        filtered_pa = log_scale(filtered_pa, scale_conf)
 
     rescale_conf = options.get("preprocessing", "rescale_type")
 